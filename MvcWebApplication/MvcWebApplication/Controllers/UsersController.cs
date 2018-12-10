1247d2c53b87ff02bdafbde1e07a923bd0ac7b1a
Tomasz Anuszczyk <tomaszanuszczyk@gmail.com>
2018-12-10 22:26:56 +0100

    Changed name of users controller and his method

---
+++ MvcWebApplication/MvcWebApplication/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using MvcWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebApplication.Controllers
+{
+    public class UsersController : Controller
+    {
+        Entities db = new Entities();
+        // GET: UsersDB
+        public ActionResult Users()
+        {
+            return View(db.AspNetUsers.ToList());
+        }
+    }
+}
