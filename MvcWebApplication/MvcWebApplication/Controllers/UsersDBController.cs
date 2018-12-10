e530e52adfaf322d8f522269be7a1ddf8c2dc84b
Tomasz Anuszczyk <tomaszanuszczyk@gmail.com>
2018-12-10 20:47:49 +0100

    Created UsersDBController

---
+++ MvcWebApplication/MvcWebApplication/Controllers/UsersDBController.cs
@@ -0,0 +1,19 @@
+﻿using MvcWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebApplication.Controllers
+{
+    public class UsersDBController : Controller
+    {
+        Entities db = new Entities();
+        // GET: UsersDB
+        public ActionResult Index()
+        {
+            return View(db.AspNetUsers.ToList());
+        }
+    }
+}
